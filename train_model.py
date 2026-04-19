09ef32cbd557dca6e08a2416deb0442d57348b22
Dorian Dayvid Gomes Feitosa <dorian.dayvid@aluno.ufca.edu.br>
2026-04-19 03:11:43 +0000

    Organização do dataset

--- train_model.py
+++ train_model.py
@@ -1,5 +1,37 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from keras.datasets import mnist
+from sklearn.model_selection import train_test_split
 
 #insira seu código aqui
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+ 
+# Dividindo o dataset de treino em treino e validação de forma balanceada
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, stratify=y_train, test_size=0.25)
+ 
+# Checando quantidade de imagens do dataset
+print('Quantidade de imagens de treino:', x_train.shape[0])
+print('Quantidade de imagens de validação:', x_val.shape[0])
+print('Quantidade de imagens de test:', x_test.shape[0])
+ 
+# Contando quantidade de imagens por dígito
+import collections
+counterTrain=collections.Counter(y_train)
+counterVal=collections.Counter(y_val)
+counterTest=collections.Counter(y_test)
+ 
+# Plotando quantidade de imagens de cada dígito
+from matplotlib import pyplot
+ 
+fig, ax = pyplot.subplots()
+rects1 = ax.bar(counterTrain.keys(), counterTrain.values(), label='Treino')
+rects2 = ax.bar(counterVal.keys(), counterVal.values(), label='Validação')
+rects3 = ax.bar(counterTest.keys(), counterTest.values(), label='Teste')
+ 
+ax.set_title('Imagens por dígito')
+ax.set_ylabel('Quantidade de imagens')
+ax.set_xlabel('Dígito')
+ax.legend()
+pyplot.savefig('distribuicao_digitos.png')
